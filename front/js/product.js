118beddc5cc1e0e485831a107c8e508a54be338b
Mathilde Laurent <mlaurent@outlook.fr>
2022-05-16 10:29:26 +0200

    Insertion du javascript pour les produits

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,48 @@
 console.log("connectée");
+
+// Recherche de l'ID dans l'URL //
+var urlParam =  new URLSearchParams(window.location.search);
+var idParam = urlParam.get('id'); 
+
+// Recherche de l'API suivant l'ID produit //
+async function productSearch() {
+    let productCard = "http://localhost:3000/api/products/"+idParam;
+    const promise = await fetch(productCard);
+    const reponseJS = await promise.json();
+
+    // Insertion de l'élément img //
+    var img = document.createElement('img');
+    img.src = reponseJS.imageUrl;
+    img.alt = reponseJS.altTxt;
+    document.querySelector('.item__img').appendChild(img);
+
+    // Insertion du nom du produit //
+    var text = document.createTextNode(reponseJS.name);
+    document.getElementById('title').appendChild(text);
+    console.log(text);
+
+    // Insertion du prix //
+    var price = document.createTextNode(reponseJS.price);
+    document.getElementById('price').appendChild(price);
+
+    // Insertion de la despcription //
+    var description = document.createTextNode(reponseJS.description);
+    document.getElementById('description').appendChild(description);
+
+    // Inserction des options de couleurs //
+    var colorOption = reponseJS.colors;
+    for (let color of colorOption) {
+        var option = document.createElement('option');
+        var optionText = document.createTextNode(color);
+        option.appendChild(optionText);
+        document.getElementById('colors').appendChild(option);
+    }
+}
+productSearch();
+
+
+
+
+
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,42 +1,46 @@
 console.log("connectée");
 
-async function donneesAPI(){
-    const resultatsAPI = await fetch("http://localhost:3000/api/products");
-    const reponseJS = await resultatsAPI.json();
-    console.log(reponseJS);
-    console.log(reponseJS[4]);
-    let numberOfProducts = reponseJS.length;
-    const items = document.getElementById("items");
-    console.log(items);
-    
-    for (let object of reponseJS) {
-      var a = document.createElement('a');
-      a.href = "./product.html?id=42";
-      items.appendChild(a);
+// Récupération des données API //
+async function apiDatas() {
+  const promise = await fetch("http://localhost:3000/api/products");
+  const reponseJS = await promise.json();
+  
+  // Implémentation du code dynamique //
+  const items = document.getElementById('items');
+  console.log(items);
+  
+  for (let object of reponseJS) {
 
-      var article = document.createElement('article');
-      a.appendChild(article);
+    // Création élément a //
+    var a = document.createElement('a');
+    a.href = `./product.html?id=${object._id}`;
+    items.appendChild(a);
 
-      var img = document.createElement('img');
-      img.src = object.imageUrl;
-      img.alt = object.altTxt;
-      article.appendChild(img);
+    // Création balise <article> //
+    var article = document.createElement('article');
+    a.appendChild(article);
 
-      var name = document.createElement('h3');
-      var text = document.createTextNode(object.name);
-      name.appendChild(text);
-      name.classList.add("productName");
-      article.appendChild(name);
+    // Création élément img //
+    var img = document.createElement('img');
+    img.src = object.imageUrl;
+    img.alt = object.altTxt;
+    article.appendChild(img);
 
-      var description = document.createElement('p');
-      var textDescription = document.createTextNode(object.description);
-      description.appendChild(textDescription);
-      description.classList.add("productDescription");
-      article.appendChild(description);
-    }
+    // Création élément H3 //
+    var name = document.createElement('h3');
+    var text = document.createTextNode(object.name);
+    name.appendChild(text);
+    name.classList.add = "productName";
+    article.appendChild(name);
+
+    // Création élément p //
+    var description = document.createElement('p');
+    var textDescription = document.createTextNode(object.description);
+    description.appendChild(textDescription);
+    description.classList.add = "productDescription";
+    article.appendChild(description);
+  }
 }
 
-donneesAPI();
-    
-        
+apiDatas();
 
